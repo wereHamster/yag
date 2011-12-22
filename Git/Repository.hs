fa1eaccc096227605b1c338d4143538530a4da6d
Tomas Carnecky <tomas.carnecky@gmail.com>
2011-12-22 03:37:09 +0100

    Can now parse and dump all objects from previous revision

--- Git/Repository.hs
+++ Git/Repository.hs
@@ -2,11 +2,14 @@
 module Git.Repository where
 
 import System.IO
+import System.Directory
+
 import Data.ByteString.Internal
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import Codec.Compression.Zlib
 
+import Data.Attoparsec.ByteString hiding (take)
 import Data.Word
 
 import Git.Hash
@@ -22,15 +25,37 @@ objectStore :: Repository -> FilePath
 objectStore repo = (repositoryDataDirectory repo) ++ "/objects"
 
 objectPath :: Repository -> Hash -> FilePath
-objectPath repo hash = (objectStore repo) ++ "/" ++ x ++ "/" ++ y
-    where
-        x = take 2 $ show hash
-        y = drop 2 $ show hash
+objectPath repo hash = (objectStore repo) ++ "/" ++ x ++ "/" ++ y where
+    x = take 2 $ show hash
+    y = drop 2 $ show hash
 
-loadObject :: Repository -> Hash -> IO S.ByteString
+loadObject :: Repository -> Hash -> IO Object
 loadObject repo hash = do
     handle <- openFile (objectPath repo hash) ReadMode
     hSetBinaryMode handle True
     contents <- L.hGetContents handle
 
-    return $ head . L.toChunks $ decompress contents
+    decompData <- return $ S.concat $ L.toChunks $ decompress contents
+    object <- return $ parseOnly gitObject decompData
+    case object of
+        Right a -> return a
+        otherwise -> error "couldn't parse object"
+
+dumpObjectAtPath :: FilePath -> IO ()
+dumpObjectAtPath path = do
+    putStrLn path
+    handle <- openFile ("test/" ++ path) ReadMode
+    hSetBinaryMode handle True
+    contents <- L.hGetContents handle
+
+    decompData <- return $ S.concat $ L.toChunks $ decompress contents
+    object <- return $ parseOnly gitObject decompData
+    case object of
+        Right a -> putStrLn (show a)
+        otherwise -> error "couldn't parse object"
+
+test :: IO ()
+test = do
+    dir <- getDirectoryContents "test"
+    mapM dumpObjectAtPath $ drop 2 dir
+    return ()
