844f4027d81f346700eaff2f23f347d06309bb73
Tomas Carnecky <tomas.carnecky@gmail.com>
2012-01-20 08:16:21 +0100

    Convert internal Hash storage to strict bytestring

--- Git/Hash.hs
+++ Git/Hash.hs
@@ -29,10 +29,10 @@ import Git.Object
 -- hash is stored in a lazy bytestring. This makes it very easy to convert it
 -- into other representations we need. At some later point, we might want to
 -- store it in its original Word160 type. But that's far future.
-data Hash = Hash { hashData :: L.ByteString } deriving (Eq)
+data Hash = Hash { hashData :: S.ByteString } deriving (Eq)
 
-hashFromHex :: L.ByteString -> Hash
-hashFromHex hex = Hash $ L.pack $ decodeHex $ L.unpack hex
+hashFromHex :: S.ByteString -> Hash
+hashFromHex hex = Hash $ S.pack $ decodeHex $ S.unpack hex
 
 
 -- * Creating hashes
@@ -40,7 +40,7 @@ hashFromHex hex = Hash $ L.pack $ decodeHex $ L.unpack hex
 -- The null hash is used quite often, for example to denote a non-existing
 -- object.
 nullHash :: Hash
-nullHash = Hash { hashData = L.pack $ take 20 $ repeat 0 }
+nullHash = Hash { hashData = S.pack $ take 20 $ repeat 0 }
 
 -- Build a Hash out of the object type and its data.
 fromObject :: Git.Object.Type -> L.ByteString -> Hash
@@ -54,20 +54,20 @@ fromObject t d =
 -- hex-encoded hash. This is most useful when reading the data from a file,
 -- network or another stream.
 fromBinaryByteString, fromHexByteString :: S.ByteString -> Hash
-fromBinaryByteString a = Hash $ L.fromChunks [a]
-fromHexByteString a    = hashFromHex $ L.fromChunks [a]
+fromBinaryByteString a = Hash a
+fromHexByteString a    = hashFromHex a
 
 -- Build a hash from an ordinary String. The hash is assumed to be
 -- hex-encoded, so this function is useful for example to parse user input.
 fromString :: String -> Hash
-fromString hex = hashFromHex $ L.pack $ map c2w hex
+fromString hex = hashFromHex $ S.pack $ map c2w hex
 
 
 -- * Internal stuff
 
-hashByteString :: Word160 -> L.ByteString
+hashByteString :: Word160 -> S.ByteString
 hashByteString hash =
-    L.pack $ concat $ map toBytes $ toWords hash
+    S.pack $ concat $ map toBytes $ toWords hash
   where
     toWords (Word160 a b c d e) = a : b : c : d : e : []
     toBytes a = (w (s a 24)) : (w (s a 16)) : (w (s a 8)) : (w a) : []
@@ -97,7 +97,7 @@ decodeHex (x:y:r) =
 
 -- Showing a hash displays its contents in hex encoding.
 instance Show Hash where
-    show hash = map w2c $ encodeHex $ L.unpack $ hashData hash
+    show hash = map w2c $ encodeHex $ S.unpack $ hashData hash
 
 abbrev :: Int -> Hash -> String
 abbrev n hash = take n $ show hash
