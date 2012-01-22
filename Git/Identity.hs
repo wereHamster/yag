ac8ec50f7ccaae1156c80ecf15b1f4e1d1037b0a
Tomas Carnecky <tomas.carnecky@gmail.com>
2012-01-22 20:23:35 +0100

    Clean up Identity

--- Git/Identity.hs
+++ Git/Identity.hs
@@ -1,20 +1,26 @@
 
-module Git.Identity where
+module Git.Identity (
+
+    -- The 'Identity' type
+    Identity(..),
+
+    -- Creating identities
+    defaultIdentity
+
+) where
 
 
-import Data.DateTime
-import System.IO
-
--- Identity is a name + email address.
+-- | Identity is a name + email address.
 data Identity = Identity { identityName :: String, identityEmail :: String } deriving (Eq)
 
-defaultIdentity :: Identity
-defaultIdentity = Identity "Tomas Carnecky" "tomas.carnecky@gmail.com"
-
-loadDefaultIdentity :: IO Identity
-loadDefaultIdentity = return defaultIdentity
-
+-- | Showing the identity will convert it to the standard format used in Git,
+-- somewhat resembling what is used in emails: A U Thor <a.u.thor@example.com>
 instance Show Identity where
     show identity = (identityName identity) ++ " <" ++ (identityEmail identity) ++ ">"
 
 
+-- | The default identity is the author of this tool. Because he is selfish.
+-- He also hopes nobody will notice and thus create commits in his name. It's
+-- part of his plan for world domination.
+defaultIdentity :: Identity
+defaultIdentity = Identity "Tomas Carnecky" "tomas.carnecky@gmail.com"
